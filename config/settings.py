0ce3e7350947471bb2554656ee28c91ae7bd0f58
Shahin Mammadov <shahinmv9@gmail.com>
2025-08-12 14:19:50 +0400

    added url

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-r$phh6(t)av+kbr8#t95kpzvc=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,deltalens-back-production.up.railway.app').split(',')
 
 
 # Application definition
